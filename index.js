055360733452853531d911ab7f6b6fea97159449
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-11-12 20:14:36 -0500

    add sentry

--- index.js
+++ index.js
@@ -2,23 +2,44 @@ require("dotenv").config(); //ver si tenemos este archivo .env y leerlo
 const Note = require("./models/Note");
 
 // const http = require("http");
+const Sentry = require("@sentry/node");
 const express = require("express");
 const cors = require("cors"); // Importa el módulo cors
-const crypto = require("crypto"); // Importa el módulo crypto
+// const crypto = require("crypto"); // Importa el módulo crypto
 const app = express();
 const logger = require("./loggerMiddleware");
+const notFound = require("./middleware/notFound.js");
+const handleErrors = require("./middleware/handleErrors.js");
 require("./mongo.js");
 
 app.use(express.json()); //Middleware que permite a express parsear el body de la request a JSON
 app.use(cors()); // Middleware que permite a express usar CORS
 app.use(logger);
-
-let notes = [];
-
-// const app = http.createServer((request, response) => {
+app.use("/images", express.static("images")); // Middleware que permite a express servir archivos estáticos
+/* const app = http.createServer((request, response) => {
 //   response.writeHead(200, { "Content-Type": "application/JSON" });
 //   response.end(JSON.stringify(notes));
-// });
+ });*/
+
+Sentry.init({
+  dsn: "https://95f975b8283ed1d7622b7211f45b517a@o4506215651016704.ingest.sentry.io/4506215677296640",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -36,26 +57,13 @@ app.get("/api/notes/:id", (request, response, next) => {
 
   Note.findById(id)
     .then((note) => {
-      if (note) {
-        return response.json(note);
-      } else {
-        response.status(404).end();
-      }
+      // si tiene la nota devuelve la nota(200)
+      return note ? response.json(note) : response.status(404).end();
+      //si no tiene la nota devuelve un 404
     })
-    .catch((error) => {
-      next(error);
-      // console.log(error.message);
-      // response.status(400).end(); //error de servicio no disponible
-    });
-
-  // console.log({ id });
-
-  // response.send(id);
-  // if (note) {
-  //   response.json(note);
-  // } else {
-  //   response.status(404).end();
-  // }
+    .catch((error) => next(error)); //si tienes un error va al siguiente middleware
+  // console.log(error.message);
+  // response.status(400).end(); //error de servicio no disponible
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
@@ -80,9 +88,7 @@ app.delete("/api/notes/:id", (request, response, next) => {
 
   const { id } = request.params;
   Note.findByIdAndDelete(id)
-    .then((result) => {
-      response.status(204).end();
-    })
+    .then(() => response.status(204).end())
     .catch((error) => next(error)); //si tienes un error va al siguiente middleware
 });
 
@@ -101,12 +107,12 @@ app.post("/api/notes", (request, response) => {
     date: new Date().toISOString(),
   });
 
-  newNote.save().then((savedNote) => {
-    // response.json(savedNote);
-    // response.status(201).json(newNote); //MAL
-    response.status(201).json(savedNote); // Solo envía la nota guardada, no newNote
-  });
-  const id = crypto.randomUUID();
+  newNote
+    .save()
+    .then((savedNote) => response.status(201).json(savedNote)) // Solo envía la nota guardada, no newNote
+    .catch((error) => next(error));
+
+  // const id = crypto.randomUUID();
   /*const ids = notes.map((note) => note.id)
 	const maxId = Math.max(...ids)*/
   console.log({ note });
@@ -119,16 +125,13 @@ app.post("/api/notes", (request, response) => {
 //   });
 // });
 
-app.use((error, request, response, next) => {
-  console.error(error);
-  console.log(error.name);
-  if (error.name === "CastError") {
-    response.status(400).send({ error: "Id no está bien usada" });
-  } else {
-    response.status(500).end();
-  }
-  // response.status(400).end();
-});
+// The error handler must be registered before any other error middleware and after all controllers
+
+app.use(notFound);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(handleErrors);
 
 const PORT = 3001;
 
